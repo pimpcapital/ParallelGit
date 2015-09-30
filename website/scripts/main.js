7919bd455cab57019424daef139aac82024c464c
beijunyi <beijunyi@gmail.com>
2015-10-01 00:56:58 +0100

    angular high lights

--- website/scripts/main.js
+++ website/scripts/main.js
@@ -1,1 +1,7 @@
-app = angular.module('pg-site', ['ngRoute', 'ui.bootstrap']);
+app = angular.module('pg-site', ['hljs', 'ngRoute', 'ui.bootstrap']);
+
+app.config(function (hljsServiceProvider) {
+  hljsServiceProvider.setOptions({
+    tabReplace: '  '
+  });
+});
