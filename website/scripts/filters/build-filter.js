2b91b6cc7fdb24449ddf85223371628bd3fdbb5d
beijunyi <beijunyi@gmail.com>
2015-10-13 20:32:38 +0100

    minor css fix

--- website/scripts/filters/build-filter.js
+++ website/scripts/filters/build-filter.js
@@ -2,7 +2,7 @@ app.filter('build', function () {
   return function (artifact, type) {
     switch(type) {
       case 'maven':
-        return '<groupId>' + artifact.groupId + '</groupId>\n  <artifactId>' + artifact.artifactId + '</artifactId>\n<version>' + artifact.version + '</version>';
+        return '<dependency>\n  <groupId>' + artifact.groupId + '</groupId>\n    <artifactId>' + artifact.artifactId + '</artifactId>\n  <version>' + artifact.version + '</version>\n</dependency>';
       case 'gradle':
         return '\'' + artifact.groupId + ':' + artifact.artifactId + ':' + artifact.version + '\'';
     }
