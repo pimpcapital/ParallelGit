5759f302d6ac866d7e8676fa9239d58b8b4bbfb8
beijunyi <beijunyi@gmail.com>
2015-11-02 22:06:17 +0000

    add examples to site

--- website/scripts/examples.js
+++ website/scripts/examples.js
@@ -16,8 +16,11 @@ app.controller('ExamplesCtrl', function($scope, $location, $timeout, $anchorScro
     module: 'utils',
     testClass: 'OpenRepositoryTest',
     examples: [{
-      name: 'Auto Detect and Open Repository',
-      method: 'autoDetectAndOpenRepository'
+      name: 'Auto Detect and Open Bare Repository',
+      method: 'autoDetectAndOpenBareRepository'
+    }, {
+      name: 'Auto Detect and Open Non-Bare Repository',
+      method: 'autoDetectAndOpenNonBareRepository'
     }]
   }, {
     name: 'Change Settings',
