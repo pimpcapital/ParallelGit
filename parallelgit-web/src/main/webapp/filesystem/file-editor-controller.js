475bbd3fc00d9e70d51234f77bf85ad452a0c643
beijunyi <beijunyi@gmail.com>
2016-02-03 23:04:59 +0000

    allow closing editor tab

--- parallelgit-web/src/main/webapp/filesystem/file-editor-controller.js
+++ parallelgit-web/src/main/webapp/filesystem/file-editor-controller.js
@@ -14,13 +14,51 @@ app.controller('FileEditorController', function($scope, WorkspaceService) {
     return request;
   }
 
-  function showFile(path, data) {
-    $scope.files[path] = data;
+  function findCurrentActiveFile() {
+    for(var i = 0; i < $scope.files.length; i++) {
+      if($scope.files[i].active)
+        return i;
+    }
+    return -1;
   }
+
+  function deactivateAll() {
+    angular.forEach($scope.files, function(file) {
+      file.active = false;
+    });
+  }
+
+  function showFile(path, data) {
+    var pos = findCurrentActiveFile() + 1;
+    deactivateAll();
+    var file;
+    for(var i = 0; i < $scope.files.length; i++) {
+      if($scope.files[i].path == path) {
+        file = $scope.files[i];
+        break;
+      }
+    }
+    if(file == null) {
+      file = {path: path, data: data};
+      $scope.files.splice(pos, 0, file);
+    }
+    file.active = true;
+  }
+
+  $scope.close = function(file) {
+    var index = $scope.files.indexOf(file);
+    if(file.active) {
+      file.active = false;
+      var neighbour = index == $scope.files.length - 1 ? $scope.files[index - 1] : $scope.files[index + 1];
+      if(neighbour != null)
+        neighbour.active = true;
+    }
+    $scope.files.splice(index, 1);
+  };
 
   $scope.$on('ready', function() {
     $scope.requests = {};
-    $scope.files = {};
+    $scope.files = [];
   });
 
   $scope.$on('open-file', function(event, path) {
