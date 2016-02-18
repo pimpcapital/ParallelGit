eff15288806c58ca4246a8d36071423f2e7429ef
Junyi Bei <beijunyi@gmail.com>
2016-02-18 13:57:52 +0000

    confirm dialog

--- parallelgit-web/src/main/webapp/system/dialog-service.js
+++ parallelgit-web/src/main/webapp/system/dialog-service.js
@@ -1,11 +1,28 @@
 app.service('DialogService', function($uibModal) {
-  this.prompt = function(text, fields) {
-    $uibModal.open({
+
+  this.confirm = function(title, message) {
+    return $uibModal.open({
+      templateUrl: 'system/confirm-dialog-modal.html',
+      size: 'sm',
+      resolve: {
+        title: function() {
+          return title;
+        },
+        message: function() {
+          return message;
+        }
+      },
+      controller : 'ConfirmDialogController'
+    });
+  };
+
+  this.prompt = function(title, fields) {
+    return $uibModal.open({
       templateUrl: 'system/prompt-dialog-modal.html',
       size: 'sm',
       resolve: {
-        text: function() {
-          return text;
+        title: function() {
+          return title;
         },
         fields: function() {
           return fields;
