1fd68379b30e651d8b5d3f29b1e9b1a88c289153
Junyi Bei <beijunyi@gmail.com>
2016-02-29 18:51:29 +0000

    code mirror linked and font size made smaller

--- parallelgit-web/src/main/webapp/filesystem/file-diffs-controller.js
+++ parallelgit-web/src/main/webapp/filesystem/file-diffs-controller.js
@@ -1,34 +1,45 @@
-app.controller('FileDiffsController', function($scope, $timeout, $uibModalInstance, src, dest) {
+app.controller('FileDiffsController', function($scope, $q, $timeout, $uibModalInstance, src, dest) {
 
-  $timeout(function() {
-    $scope.options = {
-      lineWrapping : true,
-      lineNumbers: true
-    };
-    $scope.file = {data: 'aaa'};
-
-    if(angular.isObject(src)) {
-      $scope.file = src;
-      $scope.file.acquireData();
-    } else {
-      $scope.options.origLeft = src;
-    }
-    //
-    //if(angular.isObject(dest)) {
-    //  dest.acquireData().then(function() {
-    //    $scope.file = dest;
-    //  });
-    //} else if($scope.file == null) {
-    //  $scope.options.readOnly = true;
-    //  $scope.file = {data: dest}
-    //} else {
-    //  $scope.options.origRight = dest;
-    //}
+  var promises = [prepareFile(src), prepareFile(dest)];
+  $q.all(promises).then(function(files) {
+    $timeout(function() {
+      $scope.options = {
+        lineWrapping : true,
+        lineNumbers: true
+      };
+      if(angular.isObject(files[0])) {
+        $scope.file = files[0];
+      } else {
+        $scope.options.origLeft = files[0];
+      }
+      if(angular.isObject(files[1])) {
+        $scope.file = files[1];
+      } else if($scope.file == null) {
+        $scope.file = files[1];
+        $scope.options.readOnly = true;
+      } else {
+        $scope.options.origRight = files[1];
+      }
+    });
   });
 
+  function prepareFile(file) {
+    var deferred = $q.defer();
+    if(angular.isObject(file)) {
+      file.acquireData().then(function() {
+        deferred.resolve(file);
+      });
+    } else {
+      deferred.resolve(file);
+    }
+    return deferred.promise;
+  }
+
   $scope.$watch('file.data', function(newValue, oldValue) {
-    if(oldValue != null && newValue != oldValue) console.log(newValue);
-      //$scope.file.scheduleSave();
+    if(oldValue != null && newValue != oldValue) {
+      if($scope.file.scheduleSave != null)
+        $scope.file.scheduleSave();
+    }
   });
 
   $scope.ok = function () {
@@ -39,7 +50,9 @@ app.controller('FileDiffsController', function($scope, $timeout, $uibModalInstan
     $uibModalInstance.dismiss('cancel');
   };
 
-
-
+  $scope.$on('$destroy', function() {
+     if($scope.file.releaseData != null)
+       $scope.file.releaseData();
+  });
 
 });
