f2151dd8245321dcef958f968745e91251be8d2b
beijunyi <beijunyi@gmail.com>
2016-03-02 11:46:50 +0000

    commit view and person view

--- parallelgit-web/src/main/java/com/beijunyi/parallelgit/web/protocol/GetFileRevisionsHandler.java
+++ parallelgit-web/src/main/java/com/beijunyi/parallelgit/web/protocol/GetFileRevisionsHandler.java
@@ -3,8 +3,12 @@ package com.beijunyi.parallelgit.web.protocol;
 import java.io.IOException;
 import java.util.List;
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 import com.beijunyi.parallelgit.utils.CommitUtils;
+import com.beijunyi.parallelgit.web.protocol.model.CommitView;
+import com.google.common.base.Function;
+import com.google.common.collect.Lists;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
 
@@ -21,6 +25,14 @@ public class GetFileRevisionsHandler extends AbstractRepositoryRequestHandler {
   protected ServerResponse handle(@Nonnull ClientRequest request, @Nonnull Repository repo) throws IOException {
     String path = request.getString("path");
     List<RevCommit> commits = CommitUtils.getFileRevisions(path, repo);
-    return request.respond().ok(commits);
+    List<CommitView> views = Lists.transform(commits, new Function<RevCommit, CommitView>() {
+      @Nonnull
+      @Override
+      public CommitView apply(@Nullable RevCommit revCommit) {
+        assert revCommit != null;
+        return CommitView.of(revCommit);
+      }
+    });
+    return request.respond().ok(views);
   }
 }
