9af6d9045276a398acdd2fff57addc6dfec4f986
Junyi Bei <beijunyi@gmail.com>
2016-03-11 12:13:35 +0000

    head now has attribute ref rather than branch

--- parallelgit-web/src/main/java/com/beijunyi/parallelgit/web/protocol/ListBranchesHandler.java
+++ parallelgit-web/src/main/java/com/beijunyi/parallelgit/web/protocol/ListBranchesHandler.java
@@ -3,15 +3,21 @@ package com.beijunyi.parallelgit.web.protocol;
 import java.io.IOException;
 import java.util.*;
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
-import com.beijunyi.parallelgit.utils.BranchUtils;
-import com.beijunyi.parallelgit.web.workspace.Workspace;
+import com.beijunyi.parallelgit.web.protocol.model.Head;
+import com.google.common.base.Function;
+import com.google.common.collect.Lists;
+import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
 
-import static org.eclipse.jgit.lib.Constants.MASTER;
+import static com.beijunyi.parallelgit.utils.BranchUtils.getBranches;
+import static com.google.common.collect.Collections2.transform;
+import static org.eclipse.jgit.lib.Constants.*;
 
-public class ListBranchesHandler implements RequestHandler {
+public class ListBranchesHandler extends AbstractRepositoryRequestHandler {
 
+  private static final BranchRefConverter BRANCH_REF_CONVERTER = new BranchRefConverter();
   private static final BranchSorter BRANCH_SORTER = new BranchSorter();
 
   @Nonnull
@@ -22,28 +28,64 @@ public class ListBranchesHandler implements RequestHandler {
 
   @Nonnull
   @Override
-  public ServerResponse handle(@Nonnull ClientRequest request, @Nonnull Workspace workspace) throws IOException {
-    Repository repo = workspace.getRepository();
-    return request.respond().ok(getSortedBranches(repo));
+  public ServerResponse handle(@Nonnull ClientRequest request, @Nonnull Repository repo) throws IOException {
+    List<String> branches = getSortedBranches(repo);
+    List<Head> heads = Lists.transform(branches, new BranchHeadReader(repo));
+    return request.respond().ok(heads);
   }
 
 
   @Nonnull
   private static List<String> getSortedBranches(@Nonnull Repository repo) throws IOException {
-    List<String> ret = new ArrayList<>(BranchUtils.getBranches(repo).keySet());
+    List<String> ret = new ArrayList<>(transform(getBranches(repo).values(), BRANCH_REF_CONVERTER));
     Collections.sort(ret, BRANCH_SORTER);
     return ret;
   }
 
   private static class BranchSorter implements Comparator<String> {
+
+    private static final String MASTER_REF = R_HEADS + MASTER;
+
     @Override
     public int compare(@Nonnull String b1, @Nonnull String b2) {
-      if(MASTER.equals(b1))
+      if(MASTER_REF.equals(b1))
         return -1;
-      if(MASTER.equals(b2))
+      if(MASTER_REF.equals(b2))
         return 1;
       return b1.compareTo(b2);
     }
+  }
+
+  private static class BranchRefConverter implements Function<Ref, String> {
+    @Nonnull
+    @Override
+    public String apply(@Nullable Ref ref) {
+      if(ref == null)
+        throw new IllegalStateException();
+      return ref.getName();
+    }
+  }
+
+  private static class BranchHeadReader implements Function<String, Head> {
+
+    private final Repository repo;
+
+    public BranchHeadReader(@Nonnull Repository repo) {
+      this.repo = repo;
+    }
+
+    @Nonnull
+    @Override
+    public Head apply(@Nullable String branch) {
+      if(branch == null)
+        throw new IllegalStateException();
+      try {
+        return Head.of(branch, repo);
+      } catch(IOException e) {
+        throw new RuntimeException(e);
+      }
+    }
+
   }
 
 }
