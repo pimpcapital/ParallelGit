89e66cade8f2bbf29cc9c5ac57e5ca34d0050b9f
Junyi Bei <beijunyi@gmail.com>
2016-03-11 15:50:26 +0000

    replace ng table by ui grid

--- parallelgit-web/src/main/webapp/app.js
+++ parallelgit-web/src/main/webapp/app.js
@@ -1,4 +1,4 @@
-app = angular.module('parallel', ['ngRoute', 'ngCookies', 'ui.bootstrap', 'ui.ace', 'ui.layout', 'treeControl', 'ui.tab.scroll', 'ui.bootstrap.contextMenu', 'cgNotify', 'ngTable']);
+app = angular.module('parallel', ['ngRoute', 'ngCookies', 'ui.bootstrap', 'ui.ace', 'ui.layout', 'ui.grid', 'treeControl', 'ui.tab.scroll', 'ui.bootstrap.contextMenu', 'cgNotify']);
 
 app.config(function(scrollableTabsetConfigProvider) {
   scrollableTabsetConfigProvider.setShowTooltips(false);
