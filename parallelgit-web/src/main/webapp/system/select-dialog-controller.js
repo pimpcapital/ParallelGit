d0adf66dad93cbca702b30876597f3acf9ee3bbe
beijunyi <beijunyi@gmail.com>
2016-03-12 15:40:02 +0000

    "compare with..." select done

--- parallelgit-web/src/main/webapp/system/select-dialog-controller.js
+++ parallelgit-web/src/main/webapp/system/select-dialog-controller.js
@@ -1,10 +1,27 @@
-app.controller('SelectDialogController', function($scope, title, model, data) {
+app.controller('SelectDialogController', function($scope, $uibModalInstance, title, model, data) {
+
+  $scope.title = title;
+  $scope.selected = null;
 
   $scope.grid = {
     enableRowHeaderSelection: false,
     multiSelect: false,
+    onRegisterApi: function(api) {
+      api.selection.on.rowSelectionChanged($scope, function(row) {
+        $scope.selected = row.isSelected ? row.entity : null;
+      });
+    },
     columnDefs: model,
     data: data
   };
+
+  $scope.ok = function () {
+    $uibModalInstance.close($scope.fields);
+  };
+
+  $scope.cancel = function() {
+    $uibModalInstance.dismiss('cancel');
+  };
+
 
 });
